aac5998c6c67aeb180af8eff470015c15e6df88a
Andrew Pirelli <apire001@ucr.edu>
2019-05-02 14:56:36 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@
 import tweepy
 import sys
 import json
+import os
 
 #get keys
 f = open("twitterkeys.txt", "r")
@@ -23,15 +24,41 @@ def download_tweets(user_handle, tweet_count, num_hops, output_dir):
     auth.set_access_token(access_key, access_secret)
     #use twitter API to retrieve tweets
     api = tweepy.API(auth)
-    tweets = api.user_timeline(id=user_handle,count=1,tweet_mode="extended")
-    #array to store tweet text
-    tweetText = []
-    with open ("data.json") as outfile:
-        for tweet in tweets:
-            print(tweet.full_text)
-            data = json.dump(tweet, outfile)
-        
-
+    tweets = api.user_timeline(id=user_handle,count=1,tweet_mode='extended')
+    file_cnt = 1
+    curr_filename = 'tweets_' + str(file_cnt) + '.json'
+    print(tweets[0].full_text)
+    data = {
+        "Author": tweets[0].author.name,
+        "Text": tweets[0].full_text
+    }
+    with open(curr_filename, 'w+') as outfile:
+        json.dump(data, outfile)
+    statinfo = os.stat(curr_filename)
+    print(statinfo.st_size)
+    #file_cnt = 1
+#     while(tweet_count > 0):
+#         while(len(follower_queue) > 0):
+#         if(tweet_count >= 100):
+#             user_handle = pop_front(follower_queue)
+#             tweets = api.user_timeline(id=user_handle,count=100,tweet_mode='extended')
+#             enqueueFollowers(user_handle)
+            
+#             for tweet in tweets:
+#                 curr_filename = 'tweets_' + str(file_cnt) + '.json'
+#                 statinfo = os.stat(filename)
+#                 if(statinfo.st_size > 
+#                     print(tweet.full_text)
+#                     print(tweet.author.name)
+#             tweet_count = tweet_count - len(tweets)
+            
+#         else:
+#             tweets = api.user_timeline(id=user_handle,count=tweet_count,tweet_mode='extended')
+#             for tweet in tweets:
+#                 print(tweet.full_text)
+#                 print(tweet.author.name)
+#             tweet_count = tweet_count - tweet_count
+            
 def main():
     #parse argv (command line arguments) for starting user_handle, tweet_count and num_hops
     inputs = sys.argv[1:]
