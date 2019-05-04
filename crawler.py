e7f18b61380581a3d3fda7800275c907b7abb206
Andrew Pirelli <apire001@ucr.edu>
2019-05-04 16:35:25 -0700

    Error Catching Fixed

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ from tweepy import Stream
 import sys
 import json
 from urllib.request import urlopen
+from urllib3.exceptions import ProtocolError
 import lxml.html
 import os
 
@@ -25,14 +26,18 @@ if (len(sys.argv) > 1):
     keyword = str(sys.argv[1])
 else:
     keyword = "Trump"
+
 if (len(sys.argv) > 2):
     tweet_count = int(sys.argv[2])
 else:
     tweet_count = 10
+
 if (len(sys.argv) > 3):
     dirName = str(sys.argv[3])
 else:
     dirName = "data"
+
+#generate starting filename and open filestream
 fileCount = 1
 filename = dirName + '/' + 'tweets_' + str(fileCount) + '.json'
 f = open(filename, 'w+')
@@ -44,27 +49,37 @@ class twitterListener(StreamListener):
         super(twitterListener, self).__init__()
 
     def on_data(self, data):
+        #declare scope of global variables to include twitterListener class
         global keyword
         global tweet_count
         global fileCount
         global filename
         global f
+        
+        #check if target # of tweets has been reached
         if(tweet_count <= 0):
             f.close()
             return False
+        
+        #load current tweet from stream as json object
         tweet = json.loads(data)
+        
+        #retrieve embedded URL and find its title
         if (len(tweet['entities']['urls']) != 0):
             tempURL = str(tweet['entities']['urls'][0]['expanded_url'])
         else:
             tempURL = ""
         if (tempURL != ""):
-            html = urlopen(tempURL)
-            t = lxml.html.parse(html)
-            URLtitle = str(t.find(".//title").text)
+            try:
+                html = urlopen(tempURL)
+                t = lxml.html.parse(html)
+                URLtitle = str(t.find(".//title").text)
+            except:
+                URLtitle = ""
         else:
             URLtitle = ""
-
-        #so tweets dont get truncated
+            
+        #check if tweet is extended so text is not truncated
         if 'extended_tweet' in tweet:
             if 'full_text' in tweet['extended_tweet']:
                 text = tweet['extended_tweet']['full_text']
@@ -72,19 +87,25 @@ class twitterListener(StreamListener):
                 text = tweet['text']
         elif 'text' in tweet:
             text = tweet['text']
+        
+        #store data in dict for storage
         data = {
             "Author": tweet['user']['name'],
             "Text": text,
             "Date": tweet['created_at'],
-            "Link": tempURL,
-            "Link Title": URLtitle,
+            "Embedded Link": tempURL,
+            "Embedded Link Title": URLtitle,
             "Geo": tweet['coordinates']
         }
+        
+        #Store tweet on a single line in data file
         tweet_count = tweet_count - 1
         dataString = str(json.dumps(data))
         f.write(dataString)
         f.write('\n')
         statinfo = os.stat(filename)
+        
+        #Check if 10 MB file size has been reached, create new file if true
         if(statinfo.st_size > (10 * 1024 * 1024)):
             f.close()
             ++fileCount
@@ -101,5 +122,8 @@ l = twitterListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 stream = Stream(auth, l)
-stream.filter(track=[keyword],languages=["en"])
-
+while(tweet_count > 0):
+    try:
+        stream.filter(track=[keyword],languages=["en"]) 
+    except (ProtocolError, AttributeError):
+        continue
