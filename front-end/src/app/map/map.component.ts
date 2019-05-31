4d54ac5d740599559f144bfad845c2a3543080aa
Matt <mlebo002@ucr.edu>
2019-05-30 17:03:40 -0700

    added map component

--- front-end/src/app/map/map.component.ts
+++ front-end/src/app/map/map.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  title: string = 'My Map';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
   constructor() { }
 
